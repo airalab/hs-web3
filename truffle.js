d53420a659aeccb6192ee73bb39b5d0a9c6a6761
Ilya Ostrovskiy <iostat@users.noreply.github.com>
2017-12-28 17:32:56 -0500

    remove foam internal geth CI stuff

--- truffle.js
+++ truffle.js
@@ -4,16 +4,6 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
-    },
-    rinkeby: {
-        host: "geth-rinkeby-deploy.foam.svc.cluster.local",
-        port: 8545,
-        network_id: "*" // Match any network id
-    },
-    ephemeralnet: { // required for CI testing
-        host: "EPH_POD_NAME.geth-pods.jenkins.svc.cluster.local",
-        port: 8545,
-        network_id: "*"
     }
   }
 };
