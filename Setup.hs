bca848ceda310867566ff5f750345e76bd45636d
Ilya Ostrovskiy <iostat@users.noreply.github.com>
2018-01-03 03:13:21 -0500

    Cabal 2.0+ support

--- Setup.hs
+++ Setup.hs
@@ -1,13 +1,25 @@
-import           Control.Monad                      (void, when)
-import           Data.List                          (isSuffixOf)
-import           Distribution.PackageDescription    (HookedBuildInfo, PackageDescription (testSuites), TestSuite (..))
+{-# LANGUAGE CPP #-}
+
+import           Control.Monad                             (void, when)
+import           Data.List                                 (isSuffixOf)
+import           Distribution.PackageDescription           (HookedBuildInfo, PackageDescription (testSuites),
+                                                            TestSuite (..))
 import           Distribution.Simple
-import           Distribution.Simple.LocalBuildInfo (ComponentName (..), LocalBuildInfo (..))
-import           Distribution.Simple.Setup          (BuildFlags (..), fromFlag)
+import           Distribution.Simple.LocalBuildInfo        (ComponentName (..), LocalBuildInfo (..))
+import           Distribution.Simple.Setup                 (BuildFlags (..), fromFlag)
 import           Distribution.Simple.Utils
-import           Distribution.Verbosity             (Verbosity)
-import           System.Directory                   (makeAbsolute)
-import           System.Environment                 (getEnv, getEnvironment, setEnv)
+import           Distribution.Verbosity                    (Verbosity)
+import           System.Directory                          (makeAbsolute)
+import           System.Environment                        (getEnv, getEnvironment, setEnv)
+
+#if MIN_VERSION_Cabal(2,0,0)
+
+import           Distribution.Types.ComponentName          (ComponentName (CTestName))
+import           Distribution.Types.ComponentRequestedSpec (ComponentRequestedSpec (OneComponentRequestedSpec))
+import           Distribution.Types.PackageDescription     (enabledComponents)
+import           Distribution.Types.UnqualComponentName    (mkUnqualComponentName)
+
+#endif
 
 buildingInCabal :: IO Bool
 buildingInCabal = do
@@ -15,9 +27,20 @@ buildingInCabal = do
   return $ not ("stack" `isSuffixOf` parentProcess)
 
 -- note: this only works in cabal, stack doesn't seem to pass these?
+
+#if defined(MIN_VERSION_Cabal) && MIN_VERSION_Cabal(2,0,0)
+
+willBuildLiveSuite :: PackageDescription -> Bool
+willBuildLiveSuite = not . null . liveTestComponents
+  where liveTestComponents = flip enabledComponents (OneComponentRequestedSpec (CTestName (mkUnqualComponentName "live")))
+
+#else
+
 willBuildLiveSuite :: PackageDescription  -> Bool
 willBuildLiveSuite = any isLiveTest . testSuites
   where isLiveTest t = testName t == "live" && testEnabled t
+
+#endif
 
 main :: IO ()
 main = defaultMainWithHooks simpleUserHooks
@@ -47,6 +70,7 @@ exportStore = ".detected-contract-addresses"
 
 myBuildHook :: PackageDescription -> LocalBuildInfo -> UserHooks -> BuildFlags -> IO ()
 myBuildHook pd lbi uh flags = do
+    print $ buildArgs flags
     inCabal <- buildingInCabal
     let v = fromFlag $ buildVerbosity flags
         args = buildArgs flags
