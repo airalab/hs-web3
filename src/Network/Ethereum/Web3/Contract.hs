cd04269a8243339f37c347ff55af4f715e744bc7
blinky3713 <blinky@foam.space>
2018-01-07 17:13:53 -0800

    compiling with undefined for reduceEventStream

--- src/Network/Ethereum/Web3/Contract.hs
+++ src/Network/Ethereum/Web3/Contract.hs
@@ -6,6 +6,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE RankNTypes            #-}
 {-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE ApplicativeDo  #-}
 
 -- |
 -- Module      :  Network.Ethereum.Web3.Contract
@@ -56,10 +57,7 @@ import           Control.Monad.Trans.Class      (lift)
 import           Control.Monad.Trans.Maybe      (MaybeT (..))
 import           Control.Monad.Trans.Reader             (ReaderT (..))
 import           Data.Maybe                             (listToMaybe, mapMaybe)
-import           Data.Machine                   (Is (Refl), ProcessT,
-                                                 Step (Await, Stop, Yield),
-                                                 autoT, runMachineT)
-import           Data.Machine.MealyT            (MealyT (..), arrM)
+import           Data.Machine
 import           Data.Monoid                            ((<>))
 import           Data.Proxy                             (Proxy (..))
 import qualified Data.Text                              as T
@@ -77,6 +75,14 @@ import qualified Network.Ethereum.Web3.Eth              as Eth
 import           Network.Ethereum.Web3.Provider
 import           Network.Ethereum.Web3.Types
 
+import Data.Machine.Plan
+
+
+
+--------------------------------------------------------------------------------
+-- * Event Streaming
+--------------------------------------------------------------------------------
+
 -- | Event callback control response
 data EventAction = ContinueEvent
                  -- ^ Continue to listen events
@@ -89,110 +95,15 @@ class Event e where
     -- | Event filter structure used by low-level subscription methods
     eventFilter :: Proxy e -> Address -> Filter
 
-data FilterChange a = FilterChange { filterChangeRawChange :: Change
-                                   , filterChangeEvent     :: a
-                                   }
-
--- the halting case is provided by Nothing
-type HaltingMealyT m = MealyT (MaybeT m)
-
-reduceEventStream :: forall m s a . Monad m
-                  => HaltingMealyT m s [FilterChange a]
-                  -> (a -> ReaderT Change m EventAction)
-                  -> s
-                  -> m (Maybe s)
-reduceEventStream machine handler initialState =
-  let process = autoT machine
-  in go process
-
-  where go :: ProcessT (MaybeT m) s [FilterChange a] -> m (Maybe s)
-        go process = do
-          res <- runMaybeT $ runMachineT process
-          case res of
-            Nothing   -> return $ Just initialState
-            Just Stop -> return $ Just initialState -- Pretty sure this will never get called,
-                                                    -- due to autoT for MealyT
-            Just (Yield changes next) -> do
-              acts <- processChanges handler changes
-              if TerminateEvent `notElem` acts
-              then go next
-              else return Nothing
-            Just (Await f Refl next) -> go (f initialState) >> go next -- This is a guess
-
-
-        processChanges :: (a -> ReaderT Change m EventAction) -> [FilterChange a] -> m [EventAction]
-        processChanges handler changes = forM changes $ \FilterChange{..} ->
-                                           flip runReaderT filterChangeRawChange $
-                                                handler filterChangeEvent
-
-data FilterStreamState = FilterStreamState { fssCurrentBlock  :: BlockNumber
-                                           , fssInitialFilter :: Filter
-                                           , fssWindowSize    :: Integer
-                                           }
-
-
--- | 'playLogs' streams the 'filterStream' and calls eth_getLogs on these
--- | 'Filter' objects.
-playLogs :: forall p i ni e.
-            ( Provider p
-            , DecodeEvent i ni e
-            , Event e
-            )
-         => HaltingMealyT (Web3 p) FilterStreamState [FilterChange e]
-playLogs  = do
-  filter <- filterStream
-  changes <- arrM . const . lift $ Eth.getLogs filter
-  return $ mkFilterChanges changes
-
-pollFilter :: forall p i ni e s .
-              ( Provider p
-              , DecodeEvent i ni e
-              , Event e
-              )
-           => FilterId
-           -> DefaultBlock
-           -> HaltingMealyT (Web3 p) s [FilterChange e]
-pollFilter fid stop = MealyT $ \s -> do
-  bn <- lift $ Eth.blockNumber
-  if BlockWithNumber bn > stop
-  then do
-    lift $ Eth.uninstallFilter fid
-    MaybeT $ return Nothing
-  else do
-    lift . Web3 $ threadDelay 1000
-    changes <- lift $ Eth.getFilterChanges fid
-    return (mkFilterChanges changes, pollFilter fid stop)
-
-mkFilterChanges :: forall i ni e.
-                   ( Event e
-                   , DecodeEvent i ni e
-                   )
-                => [Change]
-                -> [FilterChange e]
-mkFilterChanges cs =
-  flip mapMaybe cs $ \c@Change{..} -> do
-    x <- decodeEvent c
-    return $ FilterChange c x
-
-filterStream :: forall p . Provider p
-             => HaltingMealyT (Web3 p) FilterStreamState Filter
-filterStream = MealyT $ \FilterStreamState{..} -> do
-  end <- lift . mkBlockNumber $ filterToBlock fssInitialFilter
-  if fssCurrentBlock > end
-  then MaybeT $ return Nothing -- halt
-  else return $ let to' = newTo end fssCurrentBlock fssWindowSize
-                    filter' = fssInitialFilter { filterFromBlock = BlockWithNumber fssCurrentBlock
-                                               , filterToBlock = BlockWithNumber to'
-                                               }
-                    MealyT next = filterStream
-                in (filter', MealyT $ \s -> next s { fssCurrentBlock = succ to' })
-
-      where succ :: BlockNumber -> BlockNumber
-            succ (BlockNumber bn) = BlockNumber $ bn + 1
-
-            newTo :: BlockNumber -> BlockNumber -> Integer -> BlockNumber
-            newTo upper (BlockNumber current) window = min upper . BlockNumber $ current + window
-
+event :: forall p i ni e .
+         ( Provider p
+         , DecodeEvent i ni e
+         , Event e
+         )
+      => Filter
+      -> (e -> ReaderT Change (Web3 p) EventAction)
+      -> Web3 p (Async ())
+event fltr handler = forkWeb3 $ event' fltr 0 handler
 
 event' :: forall p i ni e .
           ( Provider p
@@ -209,7 +120,7 @@ event' fltr window handler = do
                                     , fssInitialFilter = fltr
                                     , fssWindowSize = window
                                     }
-  mLastProcessedFilterState <- reduceEventStream playLogs handler initState
+  mLastProcessedFilterState <- reduceEventStream (playLogs initState) handler
   case mLastProcessedFilterState of
     Nothing -> return ()
     Just lastProcessedFilterState -> do
@@ -217,23 +128,116 @@ event' fltr window handler = do
           pollingFromBlock = BlockNumber $ lastBlock + 1
           pollTo = filterToBlock fltr
       filterId <- Eth.newFilter fltr { filterFromBlock = BlockWithNumber pollingFromBlock }
-      void $ reduceEventStream (pollFilter filterId pollTo) handler ()
+      void $ reduceEventStream (pollFilter filterId pollTo) handler
 
-event :: forall p i ni e .
-         ( Provider p
-         , DecodeEvent i ni e
-         , Event e
-         )
-      => Filter
-      -> (e -> ReaderT Change (Web3 p) EventAction)
-      -> Web3 p (Async ())
-event fltr handler = forkWeb3 $ event' fltr 0 handler
+reduceEventStream :: Monad m
+                  => MachineT m ((->) s) [FilterChange a]
+                  -> (a -> ReaderT Change m EventAction)
+                  -> m (Maybe s)
+reduceEventStream machine handler = undefined
+  where
+    processChanges :: Monad m => (a -> ReaderT Change m EventAction) -> [FilterChange a] -> m [EventAction]
+    processChanges handler changes = forM changes $ \FilterChange{..} ->
+                                       flip runReaderT filterChangeRawChange $
+                                            handler filterChangeEvent
+
+data FilterChange a = FilterChange { filterChangeRawChange :: Change
+                                   , filterChangeEvent     :: a
+                                   }
+
+-- | 'playLogs' streams the 'filterStream' and calls eth_getLogs on these
+-- | 'Filter' objects.
+playLogs :: forall p k i ni e.
+            ( Provider p
+            , DecodeEvent i ni e
+            , Event e
+            )
+         => FilterStreamState
+         -> MachineT (Web3 p) k [FilterChange e]
+playLogs s = extractChanges (filterStream s)
+  where
+    extractChanges :: MachineT (Web3 p) k Filter -> MachineT (Web3 p) k [FilterChange e]
+    extractChanges f = stepMachine f playLog
+    playLog :: Step k Filter (MachineT (Web3 p) k Filter) -> MachineT (Web3 p) k [FilterChange e]
+    playLog step = case step of
+      Stop -> stopped
+      Await _ _ resume -> extractChanges resume
+      Yield filter resume -> emit filter <> extractChanges resume
+    emit :: Filter -> MachineT (Web3 p) k [FilterChange e]
+    emit fltr = construct $ do
+      changes <- lift . Eth.getLogs $ fltr
+      yield $ mkFilterChanges changes
+
+pollFilter :: forall p i ni e s k.
+              ( Provider p
+              , DecodeEvent i ni e
+              , Event e
+              )
+           => FilterId
+           -> DefaultBlock
+           -> MachineT (Web3 p) k [FilterChange e]
+pollFilter fid end = construct $ pollPlan fid end
+  where
+    pollPlan :: FilterId -> DefaultBlock -> PlanT k [FilterChange e] (Web3 p) ()
+    pollPlan fid end = do
+      bn <- lift $ Eth.blockNumber
+      if BlockWithNumber bn > end
+        then do
+          lift $ Eth.uninstallFilter fid
+          stop
+        else do
+          liftIO $ threadDelay 1000
+          changes <- lift $ Eth.getFilterChanges fid
+          yield $ mkFilterChanges changes
+          pollPlan fid end
+
+mkFilterChanges :: forall i ni e.
+                   ( Event e
+                   , DecodeEvent i ni e
+                   )
+                => [Change]
+                -> [FilterChange e]
+mkFilterChanges cs =
+  flip mapMaybe cs $ \c@Change{..} -> do
+    x <- decodeEvent c
+    return $ FilterChange c x
+
+data FilterStreamState = FilterStreamState { fssCurrentBlock  :: BlockNumber
+                                           , fssInitialFilter :: Filter
+                                           , fssWindowSize    :: Integer
+                                           }
+
+filterStream :: Provider p
+             => FilterStreamState
+             -> MachineT (Web3 p) k (Filter)
+filterStream initialPlan = unfoldPlan initialPlan filterPlan
+  where
+    filterPlan :: Provider p => FilterStreamState -> PlanT k Filter (Web3 p) FilterStreamState
+    filterPlan initialState@FilterStreamState{..} = do
+      end <- lift . mkBlockNumber $ filterToBlock fssInitialFilter
+      if fssCurrentBlock > end
+        then stop
+        else do
+          let to' = newTo end fssCurrentBlock fssWindowSize
+              filter' = fssInitialFilter { filterFromBlock = BlockWithNumber fssCurrentBlock
+                                         , filterToBlock = BlockWithNumber to'
+                                         }
+          yield filter'
+          filterPlan $ initialState { fssCurrentBlock = succ to' }
+    succ :: BlockNumber -> BlockNumber
+    succ (BlockNumber bn) = BlockNumber $ bn + 1
+    newTo :: BlockNumber -> BlockNumber -> Integer -> BlockNumber
+    newTo upper (BlockNumber current) window = min upper . BlockNumber $ current + window
 
 mkBlockNumber :: Provider p => DefaultBlock -> Web3 p BlockNumber
 mkBlockNumber bm = case bm of
   BlockWithNumber bn -> return bn
   Earliest -> return 0
   _ -> Eth.blockNumber
+
+--------------------------------------------------------------------------------
+-- * Transactions and Calls
+--------------------------------------------------------------------------------
 
 class Method a where
   selector :: Proxy a -> T.Text
