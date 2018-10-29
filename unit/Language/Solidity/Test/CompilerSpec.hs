badc8ea95d3bbf8ecefe9b9f7700163bcb84cba1
Alexander Krupenkin <mail@akru.me>
2018-10-29 14:16:25 +0300

    Fix compiler test when compiler is not enabled

--- unit/Language/Solidity/Test/CompilerSpec.hs
+++ unit/Language/Solidity/Test/CompilerSpec.hs
@@ -1,10 +1,14 @@
+{-# LANGUAGE CPP               #-}
 {-# LANGUAGE OverloadedLists   #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Language.Solidity.Test.CompilerSpec where
 
-import           Language.Solidity.Compiler
 import           Test.Hspec
+
+#ifdef SOLIDITY_COMPILER
+
+import           Language.Solidity.Compiler
 
 spec :: Spec
 spec = describe "solidity" $ do
@@ -19,3 +23,10 @@ spec = describe "solidity" $ do
     it "can compile simple contract" $ do
         compile (Sources [("SimpleStorage", "contract SimpleStorage { uint256 public a; function set(uint256 _a) { a = _a; } }")] [] True)
             `shouldBe` Right [("SimpleStorage", ("[\n\t{\n\t\t\"constant\" : true,\n\t\t\"inputs\" : [],\n\t\t\"name\" : \"a\",\n\t\t\"outputs\" : \n\t\t[\n\t\t\t{\n\t\t\t\t\"name\" : \"\",\n\t\t\t\t\"type\" : \"uint256\"\n\t\t\t}\n\t\t],\n\t\t\"payable\" : false,\n\t\t\"stateMutability\" : \"view\",\n\t\t\"type\" : \"function\"\n\t},\n\t{\n\t\t\"constant\" : false,\n\t\t\"inputs\" : \n\t\t[\n\t\t\t{\n\t\t\t\t\"name\" : \"_a\",\n\t\t\t\t\"type\" : \"uint256\"\n\t\t\t}\n\t\t],\n\t\t\"name\" : \"set\",\n\t\t\"outputs\" : [],\n\t\t\"payable\" : false,\n\t\t\"stateMutability\" : \"nonpayable\",\n\t\t\"type\" : \"function\"\n\t}\n]\n", "608060405234801561001057600080fd5b5060dc8061001f6000396000f3006080604052600436106049576000357c0100000000000000000000000000000000000000000000000000000000900463ffffffff1680630dbe671f14604e57806360fe47b1146076575b600080fd5b348015605957600080fd5b50606060a0565b6040518082815260200191505060405180910390f35b348015608157600080fd5b50609e6004803603810190808035906020019092919050505060a6565b005b60005481565b80600081905550505600a165627a7a7230582053764f3cc73b0960abb0d97899a8133bee5eb98c131978821f7add85f33d4f2e0029"))]
+
+#else
+
+spec :: Spec
+spec = describe "solidity compiler is not enabled" $ return ()
+
+#endif
