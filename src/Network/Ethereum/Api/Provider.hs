8c2363a8e3d31be6f3d0f62d4ad7839dadcada8c
amany9000 <amanyadavlko2502@gmail.com>
2019-06-28 01:29:48 +0530

    Pattern Matching on runWeb3', WebSocket Code Implementation in runWeb3' and adding getConnection function

--- src/Network/Ethereum/Api/Provider.hs
+++ src/Network/Ethereum/Api/Provider.hs
@@ -25,8 +25,13 @@ import           Control.Monad.State        (MonadState (..))
 import           Control.Monad.Trans.State  (StateT, evalStateT, withStateT)
 import           GHC.Generics               (Generic)
 import           Network.HTTP.Client        (Manager)
+import qualified Network.Socket             as S
+import qualified Network.WebSockets.Stream  as Stream
+import qualified Network.WebSockets         as WS ( Connection, 
+                                                    newClientConnection, 
+                                                    defaultConnectionOptions)
 
-import           Network.JsonRpc.TinyClient (JsonRpc, JsonRpcClient,
+import           Network.JsonRpc.TinyClient (JsonRpc, JsonRpcClient(..),
                                              defaultSettings, jsonRpcManager)
 
 -- | Any communication with Ethereum node wrapped with 'Web3' monad
@@ -49,11 +54,14 @@ instance Exception Web3Error
 
 --TODO: Change to `HttpProvider ServerUri | IpcProvider FilePath` to support IPC
 -- | Web3 Provider
-data Provider = HttpProvider String | WSProvider String
+data Provider = HttpProvider String | WSProvider String Int
   deriving (Show, Eq, Generic)
 
-defaultHttpPovider = HttpProvider "http://localhost:8545"
-defaultWSPovider   = WSProvider   "ws://127.0.0.1:8546"
+defaultHttpPovider :: Provider
+defaultHttpPovider = HttpProvider "http://localhost:8545" -- | Default HTTP Provider URI
+
+defaultWSPovider   :: Provider
+defaultWSPovider   = WSProvider   "127.0.0.1" 8546        -- | Default WS Provider URI 
 
 -- | 'Web3' monad runner, using the supplied Manager
 runWeb3With :: MonadIO m
@@ -66,7 +74,7 @@ runWeb3With manager provider f = do
     where
       changeManager jsonRpc = jsonRpc {jsonRpcManager = manager} 
 
--- | 'Web3' monad runner
+-- | 'Web3' monad runner for http
 runWeb3' :: MonadIO m
          => Provider
          -> Web3 a
@@ -74,6 +82,14 @@ runWeb3' :: MonadIO m
 runWeb3' (HttpProvider uri) f = do
     cfg <- defaultSettings uri
     liftIO . try . flip evalStateT cfg . unWeb3 $ f
+
+runWeb3' (WSProvider host port) f = do
+    currentConnection <- liftIO $ getConnection host port "/"
+    let currentClient = JsonRpcWSClient { 
+          jsonRpcWSConn = currentConnection
+        , jsonRpcWSHost = host
+        , jsonRpcWSPort = port}
+    liftIO . try . flip evalStateT currentClient . unWeb3 $ f
 
 -- | 'Web3' runner for default http provider
 runWeb3 :: MonadIO m
@@ -84,11 +100,40 @@ runWeb3 = runWeb3' defaultHttpPovider
 
 -- | 'Web3' runner for default WS provider
 runWeb3WS :: MonadIO m
-        => Web3 a
-        -> m (Either Web3Error a)
+          => Web3 a
+          -> m (Either Web3Error a)
 {-# INLINE runWeb3WS #-}
 runWeb3WS = runWeb3' defaultWSPovider
 
 -- | Fork 'Web3' with the same 'Provider' and 'Manager'
 forkWeb3 :: Web3 a -> Web3 (Async a)
 forkWeb3 f = liftIO . async . evalStateT (unWeb3 f) =<< get
+
+getConnection :: (Eq a, Num a, Show a)
+              => S.HostName
+              -> a
+              -> [Char]
+              -> IO WS.Connection
+{-# INLINE getConnection #-}
+getConnection host port path = do
+    -- Create and connect socket
+    let hints = S.defaultHints
+                    {S.addrSocketType = S.Stream}
+        
+        -- Correct host and path.
+        fullHost = if port == 80 then host else (host ++ ":" ++ show port)
+        path0     = if null path then "/" else path
+
+    addr:_ <- S.getAddrInfo (Just hints) (Just host) (Just $ show port)
+    sock      <- S.socket (S.addrFamily addr) S.Stream S.defaultProtocol
+    S.setSocketOption sock S.NoDelay 1
+
+    -- Connect WebSocket and run client
+     
+    res <-  ( S.connect sock (S.addrAddress addr) >>
+            Stream.makeSocketStream sock) >>=
+              (\stream ->
+                    WS.newClientConnection stream fullHost 
+                    path0 WS.defaultConnectionOptions [] )     
+    -- Clean up
+    return res
