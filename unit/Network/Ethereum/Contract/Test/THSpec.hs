7cad74b3994050e86f2dbbf861cbb5bdc5842282
Alexander Krupenkin <mail@akru.me>
2019-09-16 12:40:07 +0300

    Fix memory package bounds (commercialhaskell/stackage#4799)

--- unit/Network/Ethereum/Contract/Test/THSpec.hs
+++ unit/Network/Ethereum/Contract/Test/THSpec.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DataKinds             #-}
 {-# LANGUAGE DeriveGeneric         #-}
 {-# LANGUAGE FlexibleInstances     #-}
@@ -7,20 +6,15 @@
 {-# LANGUAGE QuasiQuotes           #-}
 module Network.Ethereum.Contract.Test.THSpec where
 
-
-import Test.Hspec
 import           Network.Ethereum.Contract.TH
-
+import           Test.Hspec
 
 -- 0x Exchange Contract that includes Tuples taken from:
 -- https://raw.githubusercontent.com/0xProject/0x-monorepo/%400x/website%400.0.89/packages/contract-artifacts/artifacts/Exchange.json
 [abiFrom|test/contracts/Exchange.json|]
 
 spec :: Spec
-spec = 
-  describe "quasi-quoter" $ 
-    it "can compile contract with tuples" $ 
+spec =
+  describe "quasi-quoter" $
+    it "can compile contract with tuples" $
       True `shouldBe` True
-    
-
-
