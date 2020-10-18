0130ed6ca2b6a02cb833948b1cdfcea451066273
Alexander Krupenkin <mail@akru.me>
2020-10-18 09:01:07 +0300

    Added web3-provider package

---
+++ packages/polkadot/src/Network/Polkadot.hs
@@ -0,0 +1,15 @@
+-- |
+-- Module      :  Network.Polkadot
+-- Copyright   :  Aleksandr Krupenkin 2016-2020
+-- License     :  Apache-2.0
+--
+-- Maintainer  :  mail@akru.me
+-- Stability   :  experimental
+-- Portability :  unportable
+--
+-- A scalable, interoperable & secure network protocol for the next web.
+--
+
+module Network.Polkadot
+    (
+    ) where
--- packages/web3/src/Network/Web3.hs
+++ packages/web3/src/Network/Web3.hs
@@ -15,6 +15,11 @@ module Network.Web3
     -- * Web3 library uses JSON-RPC over WebSocket/HTTP(S) to access node functionality.
       Web3
     , runWeb3
+    -- * Re-export popular Web3 platforms.
+    , module Network.Ethereum
+    , module Network.Polkadot
     ) where
 
+import           Network.Ethereum
+import           Network.Polkadot
 import           Network.Web3.Provider (Web3, runWeb3)
